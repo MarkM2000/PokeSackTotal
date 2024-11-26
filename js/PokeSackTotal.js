706c6a03892da40dda3482166e1aa99a35a947ae
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-11-26 12:51:24 -0500

    Create PokeSackTotal.js

---
+++ js/PokeSackTotal.js
@@ -0,0 +1,236 @@
+var PokeSackTotal = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "AL",
+          "Informant": 123,
+          "Sacks": 69,
+          "Bags": 123,
+          "Pokes": 9
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "AR",
+          "Informant": 47,
+          "Sacks": 55,
+          "Bags": 94,
+          "Pokes": 20
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "DC",
+          "Informant": 2,
+          "Sacks": 0,
+          "Bags": 2,
+          "Pokes": 0
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "DE",
+          "Informant": 12,
+          "Sacks": 1,
+          "Bags": 12,
+          "Pokes": 0
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "FL",
+          "Informant": 79,
+          "Sacks": 33,
+          "Bags": 78,
+          "Pokes": 5
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "GA",
+          "Informant": 278,
+          "Sacks": 159,
+          "Bags": 276,
+          "Pokes": 34
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "IN",
+          "Informant": 59,
+          "Sacks": 66,
+          "Bags": 58,
+          "Pokes": 12
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "KY",
+          "Informant": 74,
+          "Sacks": 79,
+          "Bags": 72,
+          "Pokes": 41
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "LA",
+          "Informant": 103,
+          "Sacks": 20,
+          "Bags": 103,
+          "Pokes": 6
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "MD",
+          "Informant": 56,
+          "Sacks": 10,
+          "Bags": 56,
+          "Pokes": 4
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "MS",
+          "Informant": 80,
+          "Sacks": 59,
+          "Bags": 80,
+          "Pokes": 1
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "NC",
+          "Informant": 99,
+          "Sacks": 72,
+          "Bags": 99,
+          "Pokes": 54
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "NJ",
+          "Informant": 46,
+          "Sacks": 1,
+          "Bags": 46,
+          "Pokes": 0
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "NY",
+          "Informant": 135,
+          "Sacks": 87,
+          "Bags": 132,
+          "Pokes": 0
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "OH",
+          "Informant": 61,
+          "Sacks": 69,
+          "Bags": 58,
+          "Pokes": 39
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "PA",
+          "Informant": 90,
+          "Sacks": 43,
+          "Bags": 90,
+          "Pokes": 63
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "SC",
+          "Informant": 147,
+          "Sacks": 94,
+          "Bags": 146,
+          "Pokes": 49
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "TN",
+          "Informant": 92,
+          "Sacks": 69,
+          "Bags": 92,
+          "Pokes": 61
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "TX",
+          "Informant": 63,
+          "Sacks": 50,
+          "Bags": 63,
+          "Pokes": 0
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "VA",
+          "Informant": 126,
+          "Sacks": 43,
+          "Bags": 126,
+          "Pokes": 50
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "State": "WV",
+          "Informant": 51,
+          "Sacks": 7,
+          "Bags": 51,
+          "Pokes": 99
+        },
+        "geometry": null
+      }
+    ]
+  }
