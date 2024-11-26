fe6be26b05bbffec2d1a762909a754549f182d66
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-11-26 13:30:34 -0500

    add JS files

---
+++ index/js/dropdown.js
@@ -0,0 +1,13 @@
+var dropdown_options = [
+    {
+        value: "neiind11",
+        text: "SackTotal"
+    },
+    {
+        value: "neiind12",
+        text: "PokeTotal"
+    },
+    {
+        value: "neiind13",
+        text: "BagTotal"
+    }]
